2573904b88da1b65ae771f9b50bd350846e33a03
Laila-dAM <lailamota.238@gmail.com>
2024-03-11 16:40:45 -0300

    The function reverses the order of words in a sentence

--- str.c
+++ str.c
@@ -12,3 +12,20 @@ void replaceCharInString(char *str, char oldChar, char newChar){
         str++;
     }
 }
+
+void reverseWordsInSentence(char *sentence) {
+    int length = strlen(sentence);
+
+    int start = 0;
+    int end = length - 1; 
+
+    while (start < end)
+    {
+        char temp = sentence[start];
+        sentence[start] = sentence[end];
+        sentence[end] = temp;
+        start++;
+        end--;
+    }
+    
+}
