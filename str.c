24558c03b9c2cab13f42cad4138953f3a6524cc6
Laila-dAM <lailamota.238@gmail.com>
2024-03-11 16:31:36 -0300

    This function swaps an old character for a new one in a string

--- str.c
+++ str.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+
+void replaceCharInString(char *str, char oldChar, char newChar){
+    while (*str != '\0'){
+        if (*str == oldChar){
+            *str = newChar;
+        }
+
+        str++;
+    }
+}
